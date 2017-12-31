c740eef0ac835490986a7c9c07fb10b818113843
WDD Fall 17 <emilyzhongsihua@gmail.com>
2017-12-31 03:33:35 -0300

    more todos

--- website2/assets/js/projects.js
+++ website2/assets/js/projects.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
 
 	1) Detects the height/width aspect ratio of the image.
 	2) Sets padding-top value for the given image to that ratio.
+
+	TODO: Currently, each time I create a new page, I need to change the
+	footer of each existing projects page.
+	Create code that autofills the footer section so I don't have to do it, 
+	taking from an array or something.
 	**/
 	$(".section-image").click(function() {
 		if (document.getElementById("lightbox")) {
