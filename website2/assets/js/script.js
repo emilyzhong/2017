ed767535c615af29af1e7bad6ab4d9c5a7724c11
WDD Fall 17 <emilyzhongsihua@gmail.com>
2018-01-08 21:18:07 -0800

    sCROLL

--- website2/assets/js/script.js
+++ website2/assets/js/script.js
@@ -142,4 +142,24 @@ $(document).ready(function() {
 			$("#contact a, #contact p").css("opacity", 1);
 		}, 600)
 	})
+
+	// Scrolllll
+
+	 // This is a functions that scrolls to #{blah}link
+	let goToByScroll = function(id) {
+	    // Remove "link" from the ID
+	    id = id.replace("-link", "");
+
+	    // Scroll
+	    $('html,body').animate({
+	        scrollTop: $("#"+id).offset().top + 20},
+	        'slow');
+	}
+
+	$("#nav > a").click(function(e) { 
+	      // Prevent a page reload when a link is pressed
+	    e.preventDefault(); 
+	      // Call the scroll function
+	    goToByScroll(this.id);           
+	});
 });
