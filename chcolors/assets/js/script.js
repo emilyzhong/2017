5b170249c5fe52dd7dde6ab51cb50e3618b17f81
WDD Fall 17 <emilyzhongsihua@gmail.com>
2018-01-12 01:42:14 -0800

    clean up code! more efficient! potential to create a reset btton

--- chcolors/assets/js/script.js
+++ chcolors/assets/js/script.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
-	
+
 	var color1 = "#97d0ed";
 	var color2 = "#3d9bc2";
 	var color3 = "#80c6dd";
 	var color4 = "#3c9bc2";
 	var color5 = "#c4e8f1";
+
+	var fills = {} // key = hex code, value = list of elements
+	var strokes = {}
+
+	var allElements = document.getElementsByTagName("svg")[0]
+									.getElementsByTagName("*");
+
+	var hexMappings = {
+		"#97d0ed": 1,
+		"#92cfe7": 1,
+		"#98cfec": 1,
+		"#3d9bc2": 2,
+		"#80c6dd": 3,
+		"#3c9bc2": 4,
+		"#3b9bc2": 4,
+		"#3f9bc1": 4,
+		"#c4e8f1": 5
+	}
 
 	$("#generate").click(function() {
 		if($("#color1").val()) {
@@ -31,47 +49,47 @@ $(document).ready(function() {
 	})
 
 	var changeColors = function() {
-		var allElements = document.getElementsByTagName("svg")[0]
-									.getElementsByTagName("*");
-		for(var i = 0; i < allElements.length; i++) {
+
+		Object.keys(fills).forEach(function(hex) {
+			if (hexMappings[hex]) {
+				let newColor = eval("color" + hexMappings[hex]);
+				let fillElements = fills[hex];
+				if (fillElements) {
+					fillElements.forEach(function(element) {
+						element.setAttribute("fill", newColor)
+					});
+				}
+
+				let strokeElements = strokes[hex];
+				if (strokeElements) {
+					strokeElements.forEach(function(element) {
+						element.setAttribute("stroke", newColor)
+					});
+				}
+			}
+		})
+	}	
+
+	var setUp = function() {
+		for (var i = 0; i < allElements.length; i++) {
 		    var element = allElements[i];
 
 		    let fill = element.getAttribute("fill");
 		    let stroke = element.getAttribute("stroke")
 
-		    // more similar colors sorry
-		    if (fill === "#97d0ed" || fill === "#92cfe7" || fill === "#98cfec") {
-		        element.setAttribute("fill", color1);
-		    } else if (stroke === "#97d0ed" || stroke === "#92cfe7" || stroke === "#98cfec") {
-		    	element.setAttribute("stroke", color1);
-		    } 
-
-		    if (fill === "#3d9bc2" ) {
-		        element.setAttribute("fill", color2);
-		    } else if (stroke === "#3d9bc2") {
-		    	element.setAttribute("stroke", color2);
-		    } 
-
-		    if (fill === "#80c6dd") {
-		        element.setAttribute("fill", color3);
-		    } else if (stroke === "#80c6dd") {
-		    	element.setAttribute("stroke", color3);
+		    if (fills[fill]) {
+		    	fills[fill].push(element)
+		    } else {
+		    	fills[fill] = [element];
 		    }
 
-		    // very similar colors all got thrown in here im so sorry
-		    if (fill === "#3c9bc2" || fill === "#3b9bc2" || fill === "#3f9bc1") {
-		        element.setAttribute("fill", color4);
-		    } else if (stroke === "#3c9bc2" || stroke === "#3b9bc2" || stroke === "#3f9bc1") {
-		    	element.setAttribute("stroke", color4);
+		    if (strokes[stroke]) {
+		    	strokes[stroke].push(element);
+		    } else {
+		    	strokes[stroke] = [element];
 		    }
-
-		    if (fill === "#c4e8f1") {
-		        element.setAttribute("fill", color5);
-		    } else if (stroke === "#c4e8f1") {
-		    	element.setAttribute("stroke", color5);
-		    }     
-		     
 		}
 	}
 
+	setUp();	
 });
