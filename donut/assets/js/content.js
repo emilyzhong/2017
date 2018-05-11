94557d91f8edb3e462fc79ccd0bfe810960fff50
Emily Zhong <emilyzhongsihua@gmail.com>
2018-05-11 12:01:51 -0700

    images

---
+++ donut/assets/js/content.js
@@ -0,0 +1,93 @@
+const SECTIONS = [
+		{
+			title: "Hi, I'm Emily",
+			imageSrc: "assets/img/me.jpg",
+			description: `
+				I'm a computer science and cognitive science student at UC Berkeley, fascinated by how technology affects people and creates experiences.
+				Here are some things I've done.
+			`
+		},
+		{
+			title: "Google",
+			imageSrc: "assets/img/google.png",
+			description: `
+				As an Engineering Practicum intern at <a href="https://google.com" target="_blank">Google</a>, 
+				I worked on the front end of internal data visualization features, 
+				using JavaScript, d3, AngularJs, and CSS.
+				`
+		},
+		{
+			title: "Re-plate Marketplace",
+			imageSrc: "assets/img/replate.jpg",
+			description: `
+				I worked in a team of five to develop a React Native and Rails application for 
+				<a href="https://www.re-plate.org" target="_blank">Re-plate</a>, 
+				a nonprofit that connects businesses with surplus food and those in need.
+				`
+		},
+		{
+			title: "Derailed",
+			imageSrc: "assets/img/derailed.png",
+			description: `
+				<a href="https://railsdecal.herokuapp.com" target="_blank">Derailed</a> 
+				is a Rails education portal that allows users and administrations to manage assignment 
+				submission and grading, classroom resources, and attendance.
+				`
+		},
+		{
+			title: "Where We Cry",
+			imageSrc: "assets/img/where-we-cry.jpg",
+			description: "For The Daily Californian's 2017 Mental Health special issue, I created a map that visualizes over 600 submissions of where and why people cry on campus."
+		},
+		{
+			title: "Cal Hacks: this is IT",
+			imageSrc: "assets/img/this-is-it.jpg",
+			description: `
+				A website for <a href="https://it.calhacks.io" target="_blank">this is IT</a>, 
+				a three part speaker-workshop series on diverse perspectives in entrepreneurship and technology. 
+				Animations created with CSS and SVGs
+			`
+		},
+		{
+			title: "Nutrition Label Redesign",
+			imageSrc: "assets/img/nutrition.jpeg",
+			description: "I redesigned the nutrition label, representing the information visually to increase comprehensiveness of what nutrients were beneficial or detrimental."
+		},
+		{
+			title: "Web Design DeCal",
+			imageSrc: "assets/img/wdd.png",
+			description: "As the head instructor of the Web Design DeCal, I've taught over 250 beginners how to design and code static websites."
+		},
+		{
+			title: "Blueprint",
+			imageSrc: "assets/img/bp.png",
+			description: "Every semester, I develop for nonprofits through Blueprint, a student organization that focuses on technology for social good."
+		},
+		{
+			title: "Cal Hacks",
+			imageSrc: "assets/img/ch.png",
+			description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
+		},
+		// {
+		// 	title: "Data Journalism @ The Daily Cal",
+		// 	imageSrc: "assets/img/ch.png",
+		// 	description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
+		// },
+		// {
+		// 	title: "Innovative Design",
+		// 	imageSrc: "assets/img/ch.png",
+		// 	description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
+		// },
+		{
+			title: "Say Hi!",
+			imageSrc: "",
+			description: `I love meeting and working with people!<br>
+				Shoot me an email at emily.zhong@berkeley.edu, or find me virtually:
+				<ul>
+					<li><a href="">LinkedIn</a></li>
+					<li><a href="">GitHub</a></li>
+				</ul>
+				`
+		},
+	]
+
--- donut/assets/js/script.js
+++ donut/assets/js/script.js
@@ -1,67 +1,4 @@
 $(document).ready(function() {
-	const SECTIONS = [
-		{
-			title: "Hi, I'm Emily",
-			imageSrc: "assets/img/me.jpg",
-			description: "I'm a computer science and cognitive science student at UC Berkeley, fascinated by how technology affects people and creates experiences.\n Here are some things I've done."
-		},
-		{
-			title: "Google",
-			imageSrc: "assets/img/google.jpg",
-			description: "As an Engineering Practicum intern at Google, I worked on the front end of internal data visualization features, using JavaScript, d3, AngularJs, and CSS."
-		},
-		{
-			title: "Re-plate Marketplace",
-			imageSrc: "assets/img/replate.jpg",
-			description: "I worked in a team of five to develop a React Native and Rails application for Re-plate, a nonprofit that connects businesses with surplus food and those in need."
-		},
-		{
-			title: "Derailed",
-			imageSrc: "assets/img/derailed.png",
-			description: "Derailed is a Rails education portal that allows users and administrations to manage assignment submission and grading, classroom resources, and attendance."
-		},
-		{
-			title: "Where We Cry",
-			imageSrc: "assets/img/where-we-cry.jpg",
-			description: "For The Daily Californian's 2017 Mental Health special issue, I created a map that visualizes over 600 submissions of where and why people cry on campus."
-		},
-		{
-			title: "Cal Hacks: this is IT",
-			imageSrc: "assets/img/this-is-it.jpg",
-			description: "A website about this is IT, a three part speaker-workshop series on diverse perspectives in entrepreneurship and technology. Animations created with CSS and SVGs"
-		},
-		{
-			title: "Nutrition Label Redesign",
-			imageSrc: "assets/img/nutrition.jpeg",
-			description: "I redesigned the nutrition label, representing the information visually to increase comprehensiveness of what nutrients were beneficial or detrimental."
-		},
-		{
-			title: "Web Design DeCal",
-			imageSrc: "assets/img/wdd.jpg",
-			description: "As the head instructor of the Web Design DeCal, I've taught over 250 beginners how to design and code static websites."
-		},
-		{
-			title: "Blueprint",
-			imageSrc: "assets/img/bp.png",
-			description: "Every semester, I develop for nonprofits through Blueprint, a student organization that focuses on technology for social good."
-		},
-		{
-			title: "Cal Hacks",
-			imageSrc: "assets/img/ch.png",
-			description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
-		},
-		// {
-		// 	title: "Data Journalism @ The Daily Cal",
-		// 	imageSrc: "assets/img/ch.png",
-		// 	description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
-		// },
-		// {
-		// 	title: "Innovative Design",
-		// 	imageSrc: "assets/img/ch.png",
-		// 	description: "I help organize the largest collegiate hackathon, Cal Hacks, with an emphasis on diversity initiatives to break entry barriers to hackathon culture."
-		// },
-	]
-
 	const WINDOW_HEIGHT = $(window).height();
 	const DOCUMENT_HEIGHT = $(document).height();
 	const NUM_SECTIONS = SECTIONS.length;
@@ -73,6 +10,11 @@ $(document).ready(function() {
 	let changeContentEnabled = true;
 	let currentOffset = 0;
 	let currIndex = null;
+
+	// setTimeout(function() {
+	// 	$("#loading-svg").fadeOut();
+	// 	$("main").fadeIn();
+	// }, 3000);
 
 	$("body").css("height", (NUM_SECTIONS + 1) * 100 + "vh");
 	changeContent();
@@ -137,8 +79,8 @@ $(document).ready(function() {
 			$("#text").removeClass("move-up");
 			sectionInfo = SECTIONS[currIndex];
 			$("#text *").stop().fadeOut(400, function() {
-				$("#title").text(sectionInfo.title);
-				$("#description").text(sectionInfo.description);
+				$("#title").html(sectionInfo.title);
+				$("#description").html(sectionInfo.description);
 				$("#text").addClass("move-up");
 			}).fadeIn(400);
 
